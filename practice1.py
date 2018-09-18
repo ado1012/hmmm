e480fe14c7ffb99a554e2056ffb34067372d3dcb
ado <2ndemail10120@gmail.com>
2018-09-18 16:04:15 +0700

    first commit

---
+++ practice1.py
@@ -0,0 +1,15 @@
+y = 0
+row = int(input("input number"))
+for x in range (1,row+1):
+    for y in range (1,x+1):
+        print ("*",end="")
+    print()
+
+ sym = *
+ mult = 1
+ count = 6
+ while mult <= count
+     print (sym*mult)
+     mult = mult + 1
+
+
